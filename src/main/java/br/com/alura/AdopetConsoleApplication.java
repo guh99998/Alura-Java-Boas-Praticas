e6196fd76e94edd1fcea25ed81ea6c0dc7d69c6f
Gustavo Lopes <guhrlopes999@gmail.com>
2023-12-13 22:27:32 -0300

    Alterada classe main

--- src/main/java/br/com/alura/AdopetConsoleApplication.java
+++ src/main/java/br/com/alura/AdopetConsoleApplication.java
@@ -1,51 +1,33 @@
 package br.com.alura;
 
-import br.com.alura.client.ClientHttpConfiguration;
-import br.com.alura.service.AbrigoService;
-import br.com.alura.service.PetService;
+import br.com.alura.command.*;
 import java.util.Scanner;
 
 public class AdopetConsoleApplication {
 
     public static void main(String[] args)
     {
-        ClientHttpConfiguration client = new ClientHttpConfiguration();
-        AbrigoService abrigoService = new AbrigoService(client);
-        PetService petService = new PetService(client);
+        CommandExecutor executor = new CommandExecutor();
+
         System.out.println("##### BOAS VINDAS AO SISTEMA ADOPET CONSOLE #####");
-        try {
+        try
+        {
             int opcaoEscolhida = 0;
             while (opcaoEscolhida != 5)
             {
-                System.out.println("\nDIGITE O NÚMERO DA OPERAÇÃO DESEJADA:");
-                System.out.println("1 -> Listar abrigos cadastrados");
-                System.out.println("2 -> Cadastrar novo abrigo");
-                System.out.println("3 -> Listar pets do abrigo");
-                System.out.println("4 -> Importar pets do abrigo");
-                System.out.println("5 -> Sair");
+                exibirMenu();
 
                 String textoDigitado = new Scanner(System.in).nextLine();
                 opcaoEscolhida = Integer.parseInt(textoDigitado);
 
-                if (opcaoEscolhida == 1)
+                switch (opcaoEscolhida)
                 {
-                    abrigoService.listarAbrigo();
-                } else if (opcaoEscolhida == 2)
-                {
-                    abrigoService.cadastrarAbrigo();
-                } else if (opcaoEscolhida == 3)
-                {
-                    petService.listarPetsAbrigo();
-                } else if (opcaoEscolhida == 4)
-                {
-                    petService.importarPetsAbrigo();
-                } else if (opcaoEscolhida == 5)
-                {
-                    break;
-                } else
-                {
-                    System.out.println("NÚMERO INVÁLIDO!");
-                    opcaoEscolhida = 0;
+                    case 1 -> executor.executeCommand(new ListarAbrigoCommand());
+                    case 2 -> executor.executeCommand(new CadastrarAbrigoCommand());
+                    case 3 -> executor.executeCommand(new ListarPetsAbrigoCommand());
+                    case 4 -> executor.executeCommand(new ImportarPetsAbrigoCommand());
+                    case 5 -> System.exit(0);
+                    default -> opcaoEscolhida = 0;
                 }
             }
             System.out.println("Finalizando o programa...");
@@ -53,5 +35,15 @@ public class AdopetConsoleApplication {
         {
             e.printStackTrace();
         }
+    }
+
+    private static void exibirMenu()
+    {
+        System.out.println("\nDIGITE O NÚMERO DA OPERAÇÃO DESEJADA:");
+        System.out.println("1 -> Listar abrigos cadastrados");
+        System.out.println("2 -> Cadastrar novo abrigo");
+        System.out.println("3 -> Listar pets do abrigo");
+        System.out.println("4 -> Importar pets do abrigo");
+        System.out.println("5 -> Sair");
     }
 }
